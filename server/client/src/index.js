8ffda3327048a118c84b95c25ebe114f9df2763c
szawruk <szawruk@gmail.com>
2020-02-26 21:49:53 +0100

    connected redux to react - provider etc

--- server/client/src/index.js
+++ server/client/src/index.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import reduxThunk from 'redux-thunk';
+
+import reducers from './reducers';
 
 import App from './components/App';
 
+const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
+
+
 ReactDOM.render(
-    <App/>,
+    <Provider store={store}>
+        <App />
+    </Provider>,
     document.querySelector('#root')
 )
