647a7b972673f332bf0a1b17656da8fd23b6b149
szawruk <szawruk@gmail.com>
2020-03-04 13:30:53 +0100

    actual weather component added

---
+++ server/client/src/components/ActualWeather.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+class ActualWeather extends React.Component {
+    render() {
+        return (
+            <div> Actual Weather</div>
+        )
+    }
+}
+
+export default ActualWeather;
--- server/client/src/components/Dashboard.js
+++ server/client/src/components/Dashboard.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ActualCity from './ActualCity';
 import DayOfTheWeek from './DayOfTheWeek';
+import ActualWeather from './ActualWeather';
 
 import '../styles/dashboard.scss';
 
@@ -8,6 +9,7 @@ const Dashboard = () => {
     return (
         <div>
             <ActualCity />
+            <ActualWeather />
             <div className='Dashboard_5days'>
                 <DayOfTheWeek day='test' />
                 <DayOfTheWeek />
